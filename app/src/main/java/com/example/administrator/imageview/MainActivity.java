020cb7c688c16c7575728b072f482ea74ff5429a
linuxalfa554 <linuxalfa554@gmail.com>
2018-05-03 15:25:00 +0800

    多少

--- app/src/main/java/com/example/administrator/imageview/MainActivity.java
+++ app/src/main/java/com/example/administrator/imageview/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.administrator.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ImageView imgView;
+    private static Button buttonSbm;
+
+    private int current_image_index;
+    int[] images = {R.mipmap.qr_codes,R.mipmap.qr_codes_round,R.mipmap.add_image1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonClick();
+    }
+
+    public void buttonClick(){
+        imgView = (ImageView)findViewById(R.id.imageView5);
+        buttonSbm =(Button)findViewById(R.id.button);
+        buttonSbm.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        current_image_index++;
+                        current_image_index = current_image_index % images.length;
+                        imgView.setImageResource(images[current_image_index]);
+                    }
+                }
+        );
     }
 }
